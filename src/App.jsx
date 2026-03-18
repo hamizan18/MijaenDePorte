db49065297bd9c4ec901a23724e07853e28bac44
hamizan18 <hamizanputra321@gmail.com>
2026-03-18 14:24:13 +0700

    adding feature image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function App() {
-  const [ notes, setNotes ] = useState([]);
+  const [image, setImage] = useState(null);
+  const [notes, setNotes ] = useState([]);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
@@ -16,18 +17,46 @@ function App() {
   }, []);
 
   async function addNote() {
+    let imageUrl = null;
+
+    if (image) {
+      const fileName = Date.now() + "-" + image.name;
+
+      const {error: uploadError } = await supabase
+        .storage
+        .from('images')
+        .upload(fileName, image);
+
+      if (uploadError) {
+        console.log("Upload error:". uploadError);
+        return;
+      }
+
+      const { data } = supabase 
+        .storage
+        .from("images")
+        .getPublicUrl(fileName);
+
+      imageUrl = data.publicUrl;
+    }
+
     const { error } = await supabase
       .from("notes")
-      .insert([{ title: title, content: content }]);
-      setTitle("")
-      setContent("")
+      .insert([
+        {
+          title: title,
+          content: content,
+          image_url: imageUrl
+        }
+      ]);
 
-      if(error) {
-        console.log(error);
-      } else {
-        getNotes();
-      }
+    if (error) {
+      console.log(error);
+    } else {
+      getNotes();
+    }
   }
+
   async function getNotes() {
     const { data } = await supabase.from("notes").select();
     setNotes(data);
@@ -35,6 +64,12 @@ function App() {
 
   return (
 <>
+
+<input
+  type="file"
+  onChange={(e) => setImage(e.target.files[0])}
+/>
+
     <input
   type="text"
   placeholder="title"
@@ -56,6 +91,10 @@ function App() {
         <div key={note.id}>
           <h3>{note.title}</h3>
           <p>{note.content}</p>
+
+          {note.image_url && (
+            <img src={note.image_url} width="200" />
+          )}
         </div>
       ))}
 
